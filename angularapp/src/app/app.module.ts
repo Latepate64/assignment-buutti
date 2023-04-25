813bef8492cc9dba3639a5d2cbd9417365343681
Lauri Koriseva <lauri.koriseva@atostek.com>
2023-04-25 09:45:24 +0300

    Scroll books infinitely

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, InfiniteScrollModule, MatCardModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
