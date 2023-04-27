1a48633fbde14a309a177f20d115c9378a675371
Lauri Koriseva <lauri.koriseva@atostek.com>
2023-04-27 10:15:16 +0300

    Started implementing tests for BookController

--- webapi/Book.cs
+++ webapi/Book.cs
@@ -2,10 +2,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace webapi;
 
-public class Book
+public class Book : IEquatable<Book>
 {
     [Key]
     public string Title { get; set; }
     public string? Author { get; set; }
     public string Timestamp { get; set; }
+
+    public bool Equals(Book? other)
+    {
+        return other != null &&
+            Title == other.Title &&
+            Author == other.Author &&
+            Timestamp == other.Timestamp;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as Book);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Title, Author, Timestamp);
+    }
 }
---
+++ webapi/Tests/BookControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using webapi.Controllers;
+using Xunit;
+
+namespace webapi.Tests
+{
+    public class BookControllerTests
+    {
+        private readonly List<Book> _books = new()
+        {
+            new Book { Author = "Aatu", Timestamp = "2000", Title = "Tarina" },
+            new Book { Author = "Seppo", Timestamp = "2010", Title = "Loru" },
+            new Book { Author = "Teemu", Timestamp = "2020", Title = "Satu" },
+        };
+
+        [Fact]
+        public void Get_ValidParameters_ShouldPass()
+        {
+            Mock<IBookService> bookService = new();
+            bookService.Setup(x => x.GetBooks()).Returns(_books);
+            BookController controller = new(bookService.Object);
+
+            List<Book>? books = (controller.Get(0, 0) as OkObjectResult).Value as List<Book>;
+
+            Assert.Equal(_books, books);
+        }
+    }
+}
